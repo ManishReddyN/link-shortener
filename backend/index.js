bfa1319f36909c7a3f4bb40c398b684641c34fd7
nmreddy1911 <manishreddy2000n@gmail.com>
2020-12-08 10:54:18 +0530

    removed all cors

--- backend/index.js
+++ backend/index.js
@@ -4,22 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require("cors");
 
 connectionString = process.env.MONGO_URL;
 const routes = require("./routes/shortlinks");
-app.use(cors());
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+
 port = process.env.PORT || 8000;
 mongoose
   .connect(connectionString, {
