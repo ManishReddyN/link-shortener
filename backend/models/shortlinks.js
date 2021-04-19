bd4c89534c20216b697c449da836b00750dac68e
nmreddy1911 <manishreddy2000n@gmail.com>
2021-04-19 14:49:57 +0530

    removed __v field

--- backend/models/shortlinks.js
+++ backend/models/shortlinks.js
@@ -1,15 +1,18 @@
 const mongoose = require("mongoose");
 
-const link = mongoose.Schema({
-  link: {
-    type: String,
-    required: true,
+const link = mongoose.Schema(
+  {
+    link: {
+      type: String,
+      required: true,
+    },
+    shortid: {
+      type: String,
+      required: true,
+      unique: true,
+    },
   },
-  shortid: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("ShortLink", link);
